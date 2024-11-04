0d5f9f5ef428a32a1fd2e2a8cbf64ed1266f4b04
Elizabeth Tzatcheva <147782076+elizabethtt@users.noreply.github.com>
2024-11-03 21:17:45 -0800

    Add files via upload

---
+++ QueueRefBased.java
@@ -0,0 +1,87 @@
+public class QueueRefBased<T> implements Queue<T> {
+
+    private QueueNode<T> front;
+    private QueueNode<T> back;
+
+    public QueueRefBased() {
+        front = null;
+        back = null;
+    }
+
+    public int size() {
+        int count = 0;
+        QueueNode<T> tmp = front;
+
+        while (tmp != null) {
+            count++;
+            tmp = tmp.next;
+        }
+        return count;
+    }
+
+
+    public boolean isEmpty() {
+        return front == null;
+    }
+
+
+    public void enqueue (T element) {
+        QueueNode<T> nn = new QueueNode<T>(element);
+        if (front == null) {
+            front = nn;
+            back = nn;
+        } else {
+            back.next = nn;
+            back = nn;
+        }
+    }
+
+    //pre condition
+    public T dequeue() throws QueueEmptyException {
+        if (front == null) {
+            throw new QueueEmptyException("thrown within dequeue");
+        }
+
+        T val = front.getValue();
+        front = front.next;
+
+        if (front == null) {
+            back = null;
+        }
+
+        return val;
+    }
+
+    // original
+    // public T dequeue() {
+
+    //     T val = front.getValue();
+    //     front = front.next;
+    //     if (front == null)
+    //         back = null;
+
+    //     return val;
+    // }
+
+    //pre condition
+    public T peek() throws QueueEmptyException {
+        if (front == null) {
+            throw new QueueEmptyException("thrown within peek");
+        }
+
+        T val = front.getValue();
+        return val;
+    }
+
+    //original
+    // public T peek()  {
+
+    //     T val = front.getValue();
+    //     return val;
+    // }
+
+    public void makeEmpty() {
+        front = null;
+        back = null;
+    }
+}
